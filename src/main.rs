cbd245f7e1a3f78f36a775ccfbf7753abda5cb26
Wilson1113 <wilsonteoshiyang@gmail.com>
2026-08-12 10:05:36 +0800

    Fix error message output to use eprintln! and add sample output file

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
